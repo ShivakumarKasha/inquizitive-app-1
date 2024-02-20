8590ec9dedeb14702a1bbbe98f4521894edd3c4b
ShivakumarKasha <shivakumarkasha12@gmail.com>
2024-02-20 12:27:30 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test updateeeee
+          my react app
         </p>
         <a
           className="App-link"
