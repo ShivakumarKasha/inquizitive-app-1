f9184ec190847b50a13dc64d0a2ec455fa77fde3
ShivakumarReliant <shivakumar@reliantvision.com>
2024-02-22 15:21:22 +0530

    added files to backend, nginx and src

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+const api = axios.create({
+  baseURL: 'http://localhost:5000', // Update with your Flask backend URL
+});
+
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    api.get('/api/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to InQUIZitive.
+          Welcome to InQUIZitive. today is wednesday
         </p>
       </header>
     </div>
@@ -15,3 +32,4 @@ function App() {
 }
 
 export default App;
+
